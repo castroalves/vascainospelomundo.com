3d810673771d55bd56deedd0aea13ad2e6112c62
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-09-26 18:26:59 +0200

    Change to published by default

--- src/pages/api/add-member.js
+++ src/pages/api/add-member.js
@@ -3,36 +3,29 @@
 import { gql, GraphQLClient } from "graphql-request";
 
 const MUTATION_ADD_MEMBER = gql`
-  mutation addMember($upsert: MemberUpsertInput!, $where: MemberWhereUniqueInput!) {
-    upsertMember(
-      upsert: $upsert,
-      where: $where
+    mutation addMember(
+        $upsert: MemberUpsertInput!
+        $where: MemberWhereUniqueInput!
     ) {
-      id
+        upsertMember(upsert: $upsert, where: $where) {
+            id
+        }
     }
-  }
 `;
 
 const MUTATION_PUBLISH_MEMBER = gql`
-  mutation publishMember($id: ID!) {
-    publishMember(
-      where: { 
-        id: $id 
+    mutation publishMember($id: ID!) {
+        publishMember(where: { id: $id }) {
+            id
         }
-    ) {
-      id
     }
-  }
 `;
 
-const hygraph = new GraphQLClient(
-  process.env.HYGRAPH_CONTENT_API,
-    {
-      headers: {
+const hygraph = new GraphQLClient(process.env.HYGRAPH_CONTENT_API, {
+    headers: {
         authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
-      },
-    }
-);
+    },
+});
 
 function getRandomInRange(from, to, fixed) {
     return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
@@ -40,43 +33,43 @@ function getRandomInRange(from, to, fixed) {
 }
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
-    // Process a POST request
-    const { firstName, lastName, phoneNumber, location, publishConsent } = req.body;
-    const upsertData = {
-      "create": {
-        "firstName": firstName,
-        "lastName": lastName,
-        "mobileNumber": phoneNumber,
-        "location": location,
-        "published": publishConsent,
-        "publishConsent": publishConsent,
-      },
-      "update": {
-        "firstName": firstName,
-        "lastName": lastName,
-        "mobileNumber": phoneNumber,
-        "location": location,
-        "published": publishConsent,
-        "publishConsent": publishConsent,
-      }
-    };
-    const whereData = {
-      id: null,
-    };
-    
-    const { upsertMember } = await hygraph.request(MUTATION_ADD_MEMBER, {
-      upsert: upsertData,
-      where: whereData,
-    });
-    
-    await hygraph.request(MUTATION_PUBLISH_MEMBER, {
-      id: upsertMember.id
-    });
-    
-    res.status(201).json({ id: upsertMember.id });
-  } else {
-    // Handle any other HTTP method
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-  }
+    if (req.method === "POST") {
+        // Process a POST request
+        const { firstName, lastName, phoneNumber, location } = req.body;
+        const upsertData = {
+            create: {
+                firstName: firstName,
+                lastName: lastName,
+                mobileNumber: phoneNumber,
+                location: location,
+                published: true,
+                publishConsent: true,
+            },
+            update: {
+                firstName: firstName,
+                lastName: lastName,
+                mobileNumber: phoneNumber,
+                location: location,
+                published: true,
+                publishConsent: true,
+            },
+        };
+        const whereData = {
+            id: null,
+        };
+
+        const { upsertMember } = await hygraph.request(MUTATION_ADD_MEMBER, {
+            upsert: upsertData,
+            where: whereData,
+        });
+
+        await hygraph.request(MUTATION_PUBLISH_MEMBER, {
+            id: upsertMember.id,
+        });
+
+        res.status(201).json({ id: upsertMember.id });
+    } else {
+        // Handle any other HTTP method
+        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+    }
 };
